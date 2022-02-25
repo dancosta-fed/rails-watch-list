e756486586e7be3441f53316d5300744f2f6e054
Daniel Costa <dancosta.id@gmail.com>
2022-02-25 17:10:02 -0300

    added seeds by hand. API wasn't working

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,14 +27,20 @@
 #             poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg',
 #             rating: 7.0)
 
-require 'json'
-require 'open-uri'
-url = 'http://tmdb.lewagon.com/movie/top_rated'
-response = URI.open(url).read
-data = JSON.parse(response)
-data['results'].each do |s|
-  Movie.create!(title: s['original_title'],
-                overview: s['overview'],
-                poster_url: "https://image.tmdb.org/t/p/w500/#{s['poster_path']}",
-                rating: s['vote_average'])
-end
+# require 'json'
+# require 'open-uri'
+# url = 'http://tmdb.lewagon.com/movie/top_rated'
+# response = URI.open(url).read
+# data = JSON.parse(response)
+# data['results'].each do |s|
+#   Movie.create!(title: s['original_title'],
+#                 overview: s['overview'],
+#                 poster_url: "https://image.tmdb.org/t/p/w500/#{s['poster_path']}",
+#                 rating: s['vote_average'])
+# end
+
+# db/seeds.rb
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
